90d09e1d4ebbb854530bfc1e1db31e748050f968
danprince <danielprince@hotmail.co.uk>
2017-06-26 01:15:40 +0100

    fix desktop menu bug

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,22 @@
 var Modal = {
   index: -1,
+
   getModalByIndex: function(index) {
     return $('[data-index=' + index + ']');
   },
+
   getCurrentModal: function() {
     return Modal.getModalByIndex(Modal.index);
   },
+
   isShowing: function() {
     return Modal.index >= 0;
   },
+
   isHidden: function() {
     return Modal.index < 0;
   },
+
   show: function(index) {
     var modal = Modal.getModalByIndex(index);
     var overlay = modal.find('.overlay');
@@ -19,6 +24,7 @@ var Modal = {
     Modal.index = index;
     overlay.fadeIn(300);
   },
+
   hide: function() {
     if (Modal.isHidden()) return;
     var modal = Modal.getCurrentModal();
@@ -27,6 +33,7 @@ var Modal = {
     Modal.index = -1;
     overlay.fadeOut(200);
   },
+
   jump: function(index) {
     if (Modal.isHidden()) return;
     var modal = Modal.getCurrentModal();
@@ -45,9 +52,11 @@ var Modal = {
       .hide()
       .fadeIn(300);
   },
+
   next: function() {
     Modal.jump(Modal.index + 1);
   },
+
   prev: function() {
     Modal.jump(Modal.index - 1);
   }
@@ -55,6 +64,7 @@ var Modal = {
 
 var Events = {
   touchStart: null,
+
   onModalClick: function(event) {
     var dataIndex = $(this).attr('data-index');
     var index = Number(dataIndex);
@@ -62,27 +72,34 @@ var Events = {
     Modal.hide();
     Modal.show(index);
   },
+
   onOverlayClick: function(event) {
     event.stopPropagation();
     if (event.target !== event.currentTarget) return;
     Modal.hide();
   },
+
   onKeyUp: function(event) {
     if (event.key === 'ArrowRight' || event.which === 39) Modal.next();
     if (event.key === 'ArrowLeft' || event.which === 37) Modal.prev();
   },
+
   onShowEnglish: function() {
     setLanguage('en');
   },
+
   onShowCymraeg: function() {
     setLanguage('cy');
   },
+
   onNextModal: function() {
     Modal.next();
   },
+
   onTouchStart: function(event) {
     Events.touchStart = event.touches[0];
   },
+
   onTouchEnd: function(event) {
     var touchEnd = event.changedTouches[0];
     var touchStart = Events.touchStart;
@@ -95,6 +112,7 @@ var Events = {
       Modal.prev();
     }
   },
+
   onToggleMobileMenu: function(event) {
     let nav = $('.site-nav');
     let showing = nav.attr('data-showing') === 'true';
